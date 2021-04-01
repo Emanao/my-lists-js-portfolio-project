be62036354c20b8bb021cbd5cff4b66d3b1415a8
emanao <emanao@me.com>
2021-04-01 19:19:16 -0400

    Seed lists

--- my-lists-backend/db/seeds.rb
+++ my-lists-backend/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+List.delete_all
+ActiveRecord::Base.connection.reset_pk_sequence!("lists")
+
+List.create(name:"Learning")
+List.create(name:"Recipes")
+List.create(name:"Movies")
