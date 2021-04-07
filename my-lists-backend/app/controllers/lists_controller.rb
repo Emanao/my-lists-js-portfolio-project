2d6bd365ed7ff943a0e1da0650bdf19cf56435e1
emanao <emanao@me.com>
2021-04-07 19:22:08 -0400

    Save a new web address to the chosen list

--- my-lists-backend/app/controllers/lists_controller.rb
+++ my-lists-backend/app/controllers/lists_controller.rb
@@ -1,13 +1,29 @@
 class ListsController < ApplicationController
     def index
         lists = List.all
-        render json: lists, only: :name
+        render json: lists, only: [:id, :name]
     end
     def create
-        new_list = List.create(list_params);
-        render json: new_list
+
+        if(!params[:id])
+
+            new_list = List.create(list_params)
+            render json: new_list
+
+        else
+            byebug
+            list = List.find(params[:id])
+            if !!list
+                list.resources.build(address: params[:address])
+                list.save
+                render json: list
+            else
+            end
+        end
+        
+        
     end
     def list_params
-        params.require(:list).permit(:name)
+        params.require(:list).permit(:id, :name)
       end
 end
