d64a91cd4e9fbcb09777432bffedcfc7d28598c1
emanao <emanao@me.com>
2021-04-06 20:35:31 -0400

    Add Resource and its migrations

---
+++ my-lists-backend/db/migrate/20210406230312_create_resources.rb
@@ -0,0 +1,10 @@
+class CreateResources < ActiveRecord::Migration[5.2]
+  def change
+    create_table :resources do |t|
+      t.string :address
+      t.references :list, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- my-lists-backend/db/schema.rb
+++ my-lists-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_01_215323) do
+ActiveRecord::Schema.define(version: 2021_04_06_230312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,13 @@ ActiveRecord::Schema.define(version: 2021_04_01_215323) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "resources", force: :cascade do |t|
+    t.string "address"
+    t.bigint "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_resources_on_list_id"
+  end
+
+  add_foreign_key "resources", "lists"
 end
